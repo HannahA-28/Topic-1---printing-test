c962f885c730b469eb4023e97f1eca139782acb1
339729345 <339729345@2260I-L29-007>
2025-02-04 14:03:45 -0500

    Add project files.

---
+++ Topic 1 - printing test/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_1___printing_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hannah Anderson
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This will print on a new line");
+
+            Console.WriteLine();
+
+            Console.Write("Hello World!");
+            Console.Write("This will print on the same line");
+        }
+    }
+}
